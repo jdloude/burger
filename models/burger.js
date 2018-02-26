4aa6064672a39902f086bcbc4fc1a2158d3cef6d
Joshua Loudermilk <jdloude@gmail.com>
2018-02-25 21:23:37 -0700

    v1.1

--- models/burger.js
+++ models/burger.js
@@ -1,23 +1,22 @@
-
 var orm = require('../config/orm.js');
 
 var burger = {
     all: function(cb) {
-        orm.selectAll('burgers', function(res){
+        orm.selectAll('burgers', function(res) {
             cb(res);
-            // console.log(res);
+
         })
-    },   
+    },
     create: function(newBurger, cb) {
-        orm.insertOne('burgers',newBurger, function(res){
+        orm.insertOne('burgers', newBurger, function(res) {
             cb(res);
-            // console.log(res);            
+
         })
-    },   
-    update: function(col,row,cb) {
-        orm.updateOne('burgers',col,row, function(res){
+    },
+    update: function(col, row, cb) {
+        orm.updateOne('burgers', col, row, function(res) {
             cb(res);
-            // console.log(res);            
+
         })
     }
 }
