a6c809438e67a7c2b7e54229d50c4fcd3a2268fa
Joshua Loudermilk <jdloude@gmail.com>
2018-02-25 21:36:34 -0700

    v1.1

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,23 @@
 //set up db connection
 const mysql = require('mysql');
 
-var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '1234',
-    database: 'burgers_db'
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    var connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '1234',
+        database: 'burgers_db'
+    });
 
-// Make connection.
-connection.connect(function(err) {
-    if (err) {
-        console.error('error connecting: ', err.stack);
-        return;
-    }
-    console.log('mySQL is running on ID:', connection.threadId);
-});
+    // Make connection.
+    connection.connect(function(err) {
+        if (err) {
+            console.error('error connecting: ', err.stack);
+            return;
+        }
+        console.log('mySQL is running on ID:', connection.threadId);
+    });
+}
 module.exports = connection;
